ff5aa44527c98b6497d748b3bf017b21a0e83e81
GitHub Action <action@github.com>
2025-10-27 22:43:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/theenderlime/mytelegramplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/theenderlime/mytelegramplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.theenderlime.mytelegramplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.theenderlime.mytelegramplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
